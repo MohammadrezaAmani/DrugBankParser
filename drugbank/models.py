b73ee00918e095a7d5ab49fb88ebc585e58c04ea
MohammadrezaAmani <More.amani@yahoo.com>
2023-12-05 14:40:14 +0330

    minor changes.

--- drugbank/models.py
+++ drugbank/models.py
@@ -57,7 +57,7 @@ class Drug:
         carriers,
         transporters,
         type_,
-    ):
+    ) -> None:
         self.drugbankid = drugbankid
         self.name = name
         self.description = description
@@ -117,7 +117,7 @@ class Drug:
 
 
 class Groups:
-    def __init__(self, group):
+    def __init__(self, group) -> None:
         self.group = group
 
 
@@ -134,7 +134,7 @@ class Classification:
         subclass,
         alternativeparent,
         substituent,
-    ):
+    ) -> None:
         self.description = description
         self.directparent = directparent
         self.kingdom = kingdom
@@ -146,25 +146,25 @@ class Classification:
 
 
 class Synonyms:
-    def __init__(self, synonym):
+    def __init__(self, synonym) -> None:
         self.synonym = synonym
 
 
 class Synonym:
-    def __init__(self, language, coder):
+    def __init__(self, language, coder) -> None:
         self.language = language
         self.coder = coder
 
 
 class Salts:
-    def __init__(self, salt):
+    def __init__(self, salt) -> None:
         self.salt = salt
 
 
 class Salt:
     def __init__(
         self, drugbankid, name, unii, casnumber, inchikey, averagemass, monoisotopicmass
-    ):
+    ) -> None:
         self.drugbankid = drugbankid
         self.name = name
         self.unii = unii
@@ -177,69 +177,69 @@ class Salt:
 class Drugbankdrugsaltid:
     """The DrugBank ID is used to uniquely identify a drug or salt entry. There is a primary ID and several secondary IDs that come from older ID formats or merged entries."""
 
-    def __init__(self, primary):
+    def __init__(self, primary) -> None:
         self.primary = primary
 
 
 class Drugbankmetaboliteid:
     """The metabolite DrugBank ID uniquely identifies a metabolite entry. Multiple IDs indicate a merged entry."""
 
-    def __init__(self, primary):
+    def __init__(self, primary) -> None:
         self.primary = primary
 
 
 class Article:
-    def __init__(self, refid, pubmedid, citation):
+    def __init__(self, refid, pubmedid, citation) -> None:
         self.refid = refid
         self.pubmedid = pubmedid
         self.citation = citation
 
 
 class Textbook:
-    def __init__(self, refid, isbn, citation):
+    def __init__(self, refid, isbn, citation) -> None:
         self.refid = refid
         self.isbn = isbn
         self.citation = citation
 
 
 class Link:
-    def __init__(self, refid, title, url):
+    def __init__(self, refid, title, url) -> None:
         self.refid = refid
         self.title = title
         self.url = url
 
 
 class Attachment:
-    def __init__(self, refid, title, url):
+    def __init__(self, refid, title, url) -> None:
         self.refid = refid
         self.title = title
         self.url = url
 
 
 class Articles:
-    def __init__(self, article):
+    def __init__(self, article) -> None:
         self.article = article
 
 
 class Textbooks:
-    def __init__(self, textbook):
+    def __init__(self, textbook) -> None:
         self.textbook = textbook
 
 
 class Links:
-    def __init__(self, link):
+    def __init__(self, link) -> None:
         self.link = link
 
 
 class Attachments:
-    def __init__(self, attachment):
+    def __init__(self, attachment) -> None:
         self.attachment = attachment
 
 
 class References:
     """Articles are typically PubMed references, although there are some references which do not have a PubMed ID."""
 
-    def __init__(self, articles, textbooks, links, attachments):
+    def __init__(self, articles, textbooks, links, attachments) -> None:
         self.articles = articles
         self.textbooks = textbooks
         self.links = links
@@ -247,18 +247,18 @@ class References:
 
 
 class Internationalbrands:
-    def __init__(self, internationalbrand):
+    def __init__(self, internationalbrand) -> None:
         self.internationalbrand = internationalbrand
 
 
 class Internationalbrand:
-    def __init__(self, name, company):
+    def __init__(self, name, company) -> None:
         self.name = name
         self.company = company
 
 
 class Products:
-    def __init__(self, product):
+    def __init__(self, product) -> None:
         self.product = product
 
 
@@ -285,7 +285,7 @@ class Product:
         approved,
         country,
         source,
-    ):
+    ) -> None:
         self.name = name
         self.labeller = labeller
         self.ndcid = ndcid
@@ -307,114 +307,114 @@ class Product:
 
 
 class Mixtures:
-    def __init__(self, mixture):
+    def __init__(self, mixture) -> None:
         self.mixture = mixture
 
 
 class Mixture:
-    def __init__(self, name, ingredients, supplementalingredients):
+    def __init__(self, name, ingredients, supplementalingredients) -> None:
         self.name = name
         self.ingredients = ingredients
         self.supplementalingredients = supplementalingredients
 
 
 class Packagers:
-    def __init__(self, packager):
+    def __init__(self, packager) -> None:
         self.packager = packager
 
 
 class Packager:
-    def __init__(self, name, url):
+    def __init__(self, name, url) -> None:
         self.name = name
         self.url = url
 
 
 class Manufacturers:
-    def __init__(self, manufacturer):
+    def __init__(self, manufacturer) -> None:
         self.manufacturer = manufacturer
 
 
 class Manufacturer:
-    def __init__(self, generic, url):
+    def __init__(self, generic, url) -> None:
         self.generic = generic
         self.url = url
 
 
 class Prices:
-    def __init__(self, price):
+    def __init__(self, price) -> None:
         self.price = price
 
 
 class Price:
     """The price for the given drug in US or Canadian currency."""
 
-    def __init__(self, description, cost, currency):
+    def __init__(self, description, cost, currency) -> None:
         self.description = description
         self.cost = cost
         self.currency = currency
 
 
 class Categorys:
-    def __init__(self, category):
+    def __init__(self, category) -> None:
         self.category = category
 
 
 class Category:
-    def __init__(self, category, meshid):
+    def __init__(self, category, meshid) -> None:
         self.category = category
         self.meshid = meshid
 
 
 class Affectedorganisms:
-    def __init__(self, affectedorganism):
+    def __init__(self, affectedorganism) -> None:
         self.affectedorganism = affectedorganism
 
 
 class Dosages:
-    def __init__(self, dosage):
+    def __init__(self, dosage) -> None:
         self.dosage = dosage
 
 
 class Dosage:
-    def __init__(self, form, route, strength):
+    def __init__(self, form, route, strength) -> None:
         self.form = form
         self.route = route
         self.strength = strength
 
 
 class Atccodes:
-    def __init__(self, atccode):
+    def __init__(self, atccode) -> None:
         self.atccode = atccode
 
 
 class Atccode:
-    def __init__(self, level, code):
+    def __init__(self, level, code) -> None:
         self.level = level
         self.code = code
 
 
 class Atccodelevel:
-    def __init__(self, code):
+    def __init__(self, code) -> None:
         self.code = code
 
 
 class Ahfscodes:
-    def __init__(self, ahfscode):
+    def __init__(self, ahfscode) -> None:
         self.ahfscode = ahfscode
 
 
 class Pdbentrys:
-    def __init__(self, pdbentry):
+    def __init__(self, pdbentry) -> None:
         self.pdbentry = pdbentry
 
 
 class Patents:
-    def __init__(self, patent):
+    def __init__(self, patent) -> None:
         self.patent = patent
 
 
 class Patent:
-    def __init__(self, number, country, approved, expires, pediatricextension):
+    def __init__(self, number, country, approved, expires, pediatricextension) -> None:
         self.number = number
         self.country = country
         self.approved = approved
@@ -423,81 +423,81 @@ class Patent:
 
 
 class Foodinteractions:
-    def __init__(self, foodinteraction):
+    def __init__(self, foodinteraction) -> None:
         self.foodinteraction = foodinteraction
 
 
 class Druginteractions:
-    def __init__(self, druginteraction):
+    def __init__(self, druginteraction) -> None:
         self.druginteraction = druginteraction
 
 
 class Druginteraction:
-    def __init__(self, drugbankid, name, description):
+    def __init__(self, drugbankid, name, description) -> None:
         self.drugbankid = drugbankid
         self.name = name
         self.description = description
 
 
 class Sequences:
-    def __init__(self, sequence, format):
+    def __init__(self, sequence, format) -> None:
         self.sequence = sequence
         self.format = format
 
 
 class Calculatedpropertys:
-    def __init__(self, property):
+    def __init__(self, property) -> None:
         self.property = property
 
 
 class Calculatedproperty:
-    def __init__(self, kind, value, source):
+    def __init__(self, kind, value, source) -> None:
         self.kind = kind
         self.value = value
         self.source = source
 
 
 class Experimentalpropertys:
-    def __init__(self, property):
+    def __init__(self, property) -> None:
         self.property = property
 
 
 class Experimentalproperty:
-    def __init__(self, kind, value, source):
+    def __init__(self, kind, value, source) -> None:
         self.kind = kind
         self.value = value
         self.source = source
 
 
 class Externalidentifiers:
-    def __init__(self, externalidentifier):
+    def __init__(self, externalidentifier) -> None:
         self.externalidentifier = externalidentifier
 
 
 class Externalidentifier:
-    def __init__(self, resource, identifier):
+    def __init__(self, resource, identifier) -> None:
         self.resource = resource
         self.identifier = identifier
 
 
 class Externallinks:
-    def __init__(self, externallink):
+    def __init__(self, externallink) -> None:
         self.externallink = externallink
 
 
 class Externallink:
-    def __init__(self, resource, url):
+    def __init__(self, resource, url) -> None:
         self.resource = resource
         self.url = url
 
 
 class Pathways:
-    def __init__(self, pathway):
+    def __init__(self, pathway) -> None:
         self.pathway = pathway
 
 
 class Pathway:
-    def __init__(self, smpdbid, name, category, drugs, enzymes):
+    def __init__(self, smpdbid, name, category, drugs, enzymes) -> None:
         self.smpdbid = smpdbid
         self.name = name
         self.category = category
@@ -506,28 +506,28 @@ class Pathway:
 
 
 class Pathwaydrugs:
-    def __init__(self, drug):
+    def __init__(self, drug) -> None:
         self.drug = drug
 
 
 class Pathwaydrug:
-    def __init__(self, drugbankid, name):
+    def __init__(self, drugbankid, name) -> None:
         self.drugbankid = drugbankid
         self.name = name
 
 
 class Pathwayenzymes:
-    def __init__(self, uniprotid):
+    def __init__(self, uniprotid) -> None:
         self.uniprotid = uniprotid
 
 
 class Reactions:
-    def __init__(self, reaction):
+    def __init__(self, reaction) -> None:
         self.reaction = reaction
 
 
 class Reaction:
-    def __init__(self, sequence, leftelement, rightelement, enzymes):
+    def __init__(self, sequence, leftelement, rightelement, enzymes) -> None:
         self.sequence = sequence
         self.leftelement = leftelement
         self.rightelement = rightelement
@@ -535,25 +535,25 @@ class Reaction:
 
 
 class Reactionelement:
-    def __init__(self, drugbankid, name):
+    def __init__(self, drugbankid, name) -> None:
         self.drugbankid = drugbankid
         self.name = name
 
 
 class Reactionenzymes:
-    def __init__(self, enzyme):
+    def __init__(self, enzyme) -> None:
         self.enzyme = enzyme
 
 
 class Reactionenzyme:
-    def __init__(self, drugbankid, name, uniprotid):
+    def __init__(self, drugbankid, name, uniprotid) -> None:
         self.drugbankid = drugbankid
         self.name = name
         self.uniprotid = uniprotid
 
 
 class Snpeffects:
-    def __init__(self, effect):
+    def __init__(self, effect) -> None:
         self.effect = effect
 
 
@@ -568,7 +568,7 @@ class Snpeffect:
         definingchange,
         description,
         pubmedid,
-    ):
+    ) -> None:
         self.proteinname = proteinname
         self.genesymbol = genesymbol
         self.uniprotid = uniprotid
@@ -580,7 +580,7 @@ class Snpeffect:
 
 
 class Snpadversedrugreactions:
-    def __init__(self, reaction):
+    def __init__(self, reaction) -> None:
         self.reaction = reaction
 
 
@@ -595,7 +595,7 @@ class Snpadversedrugreaction:
         adversereaction,
         description,
         pubmedid,
-    ):
+    ) -> None:
         self.proteinname = proteinname
         self.genesymbol = genesymbol
         self.uniprotid = uniprotid
@@ -607,7 +607,7 @@ class Snpadversedrugreaction:
 
 
 class Polypeptides:
-    def __init__(self, polypeptide):
+    def __init__(self, polypeptide) -> None:
         self.polypeptide = polypeptide
 
 
@@ -627,7 +627,7 @@ class Polypeptide:
         chromosomelocation,
         organism,
         ncbitaxonomyid,
-    ):
+    ) -> None:
         self.name = name
         self.generalfunction = generalfunction
         self.specificfunction = specificfunction
@@ -653,7 +653,7 @@ class Organism:
         pfams,
         goclassifiersid,
         source,
-    ):
+    ) -> None:
         self.externalidentifiers = externalidentifiers
         self.synonyms = synonyms
         self.aminoacidsequence = aminoacidsequence
@@ -664,73 +664,73 @@ class Organism:
 
 
 class Polypeptideexternalidentifiers:
-    def __init__(self, externalidentifier):
+    def __init__(self, externalidentifier) -> None:
         self.externalidentifier = externalidentifier
 
 
 class Polypeptideexternalidentifier:
-    def __init__(self, resource, identifier):
+    def __init__(self, resource, identifier) -> None:
         self.resource = resource
         self.identifier = identifier
 
 
 class Polypeptidesynonyms:
-    def __init__(self, synonym):
+    def __init__(self, synonym) -> None:
         self.synonym = synonym
 
 
 class Sequence:
-    def __init__(self, format):
+    def __init__(self, format) -> None:
         self.format = format
 
 
 class Pfams:
-    def __init__(self, pfam):
+    def __init__(self, pfam) -> None:
         self.pfam = pfam
 
 
 class Pfam:
-    def __init__(self, identifier, name):
+    def __init__(self, identifier, name) -> None:
         self.identifier = identifier
         self.name = name
 
 
 class Goclassifiers:
-    def __init__(self, goclassifier):
+    def __init__(self, goclassifier) -> None:
         self.goclassifier = goclassifier
 
 
 class Goclassifier:
-    def __init__(self, category, description):
+    def __init__(self, category, description) -> None:
         self.category = category
         self.description = description
 
 
 class Actions:
-    def __init__(self, action):
+    def __init__(self, action) -> None:
         self.action = action
 
 
 class Targets:
-    def __init__(self, target):
+    def __init__(self, target) -> None:
         self.target = target
 
 
 class Target:
-    def __init__(self, interactantgroup, position):
+    def __init__(self, interactantgroup, position) -> None:
         self.interactantgroup = interactantgroup
         self.position = position
 
 
 class Enzymes:
-    def __init__(self, enzyme):
+    def __init__(self, enzyme) -> None:
         self.enzyme = enzyme
 
 
 class Enzyme:
     def __init__(
         self, interactantgroup, inhibitionstrength, inductionstrength, position
-    ):
+    ) -> None:
         self.interactantgroup = interactantgroup
         self.inhibitionstrength = inhibitionstrength
         self.inductionstrength = inductionstrength
@@ -738,22 +738,22 @@ class Enzyme:
 
 
 class Carriers:
-    def __init__(self, carrier):
+    def __init__(self, carrier) -> None:
         self.carrier = carrier
 
 
 class Carrier:
-    def __init__(self, interactantgroup, position):
+    def __init__(self, interactantgroup, position) -> None:
         self.interactantgroup = interactantgroup
         self.position = position
 
 
 class Transporters:
-    def __init__(self, transporter):
+    def __init__(self, transporter) -> None:
         self.transporter = transporter
 
 
 class Transporter:
-    def __init__(self, interactantgroup, position):
+    def __init__(self, interactantgroup, position) -> None:
         self.interactantgroup = interactantgroup
         self.position = position
