b9504f586da6a1357aefd4dee5f84b813c910d0d
MohammadrezaAmani <More.amani@yahoo.com>
2023-12-05 13:17:55 +0330

    update regex patterns

---
+++ drugbank/patterns.py
@@ -0,0 +1,12 @@
+class Drugbankmetaboliteidvalue:
+    DBMET = r"DBMET[0-9]{5}"
+
+
+class Drugbankdrugsaltidvalue:
+    DB = r"DB[0-9]{5}"
+    DBSALT = r"DBSALT[0-9]{6}"
+    APRD = r"APRD[0-9]{5}"
+    BIOD = r"BIOD[0-9]{5}"
+    BTD = r"BTD[0-9]{5}"
+    EXPT = r"EXPT[0-9]{5}"
+    NUTR = r"NUTR[0-9]{5}"
